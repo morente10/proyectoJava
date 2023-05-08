4e115478dd784bb257194a53e2fc5e56ea5c716e
Antonio Morente <55946043+profemorente@users.noreply.github.com>
2023-05-08 10:11:39 +0200

    p

--- src/Nombre.java
+++
@@ -1,7 +0,0 @@
-public class Nombre {
-    public static void main(String[] args) {
-        String nombre="Antonio Morente";
-        var edad=40;
-        System.out.println("Mi nombre es"+nombre+" edad: "+edad);
-    }
-}
